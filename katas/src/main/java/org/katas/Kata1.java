cb93dc06cab9367cb90983b4bb4841a10b977466
Silvia Lupsa <lupsasilvia@gmail.com>
2023-08-11 13:29:38 +0300

    folder created

---
+++ katas/src/main/java/org/katas/Kata1.java
@@ -0,0 +1,6 @@
+package org.katas;
+
+
+//todo: 
+public class Kata1 {
+}
---
+++ katas/src/main/java/org/katas/Main.java
@@ -0,0 +1,11 @@
+package org.katas;
+
+import org.katas.Kata1;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
