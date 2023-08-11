3c495a3a0359cbac9ffafa103d9320a93d78c020
Silvia Lupsa <lupsasilvia@gmail.com>
2023-08-11 14:34:57 +0300

    kata1 done

--- katas/src/main/java/org/katas/Main.java
+++ katas/src/main/java/org/katas/Main.java
@@ -5,7 +5,7 @@ import org.katas.Kata1;
 public class Main {
     public static void main(String[] args) {
 
-
-
+        int[] result1 = Kata1.flip('R', new int[]{ 3,2,1,2 });
+        int[] result2 =  Kata1.flip('L', new int[]{ 1,4,5,3,5 });
     }
 }
