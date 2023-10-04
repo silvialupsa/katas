8c67a952983255fafbb362389170d6f0ec189c11
Silvia Lupsa <lupsasilvia@gmail.com>
2023-10-04 17:10:35 +0300

    array diff 29

--- katas/src/main/java/org/katas/Main.java
+++ katas/src/main/java/org/katas/Main.java
@@ -26,6 +26,7 @@ public class Main {
 //    TakeATenMinutesWalk_25.main();
 //        WhoLikesIt_26.main();
 //        StopGninnipSMySdroW_27.main();
-        FindOdd_28.main();
+//        FindOdd_28.main();
+        ArrayDiff_29.main();
     }
 }
